3a92d0dfe081923c405613624efaef0615b135ca
emanuks <emanuels313@gmail.com>
2024-04-18 14:33:40 -0300

    feat: adding more complex tests and implementation of cases like IX, CD and CM

--- roman_numerals.cpp
+++ roman_numerals.cpp
@@ -30,11 +30,11 @@ int get_roman_numeral_priority(char numeral) {
     switch (numeral) {
         case 'I': return 1;
         case 'V': return 2;
-        case 'X': return 3;
-        case 'L': return 4;
-        case 'C': return 5;
-        case 'D': return 6;
-        case 'M': return 7;
+        case 'X': return 2;
+        case 'L': return 3;
+        case 'C': return 3;
+        case 'D': return 4;
+        case 'M': return 4;
     }
 
     return -1;
@@ -48,29 +48,36 @@ int roman_numerals_to_decimal(std::string roman_numerals) {
     int decimal = 0;
     int occurrence_qty = 0;
     int priority = -1;
+    int previous_character = 'I';
     std::reverse(roman_numerals.begin(), roman_numerals.end());
 
     std::string::iterator c;
     for (c = roman_numerals.begin(); c != roman_numerals.end(); ++c) {
         int current_value = get_roman_numeral_value(*c);
         int current_priority = get_roman_numeral_priority(*c);
+        int previous_priority = get_roman_numeral_priority(previous_character);
 
-        if (current_priority != priority) {
-            priority = current_priority;
+        if (current_value == -1)
+            return -1;
+
+        if (*c != previous_character) {
+            if (current_priority == previous_priority - 1
+                    && can_have_multiple_consecutive_occurrences(*c)) {
+                current_value *= -1;
+            }
+
+            previous_character = *c;
             occurrence_qty = 0;
         }
 
         occurrence_qty++;
         if (can_have_multiple_consecutive_occurrences(*c) && occurrence_qty > 3)
             return -1;
-        else if (!can_have_multiple_consecutive_occurrences(*c) && occurrence_qty > 1)
+        else if (!can_have_multiple_consecutive_occurrences(*c)
+                    && occurrence_qty > 1)
             return -1;
 
-        if (current_value == -1) {
-            return -1;
-        } else {
-            decimal += current_value;
-        }
+        decimal += current_value;
     }
 
     return decimal;
--- roman_numerals_test.cpp
+++ roman_numerals_test.cpp
@@ -17,23 +17,77 @@ TEST_CASE("Roman Numerals - Valid", "[roman_numerals]") {
     REQUIRE(roman_numerals_to_decimal("II") == 2);
     REQUIRE(roman_numerals_to_decimal("III") == 3);
 
+    REQUIRE(roman_numerals_to_decimal("IV") == 4);
     REQUIRE(roman_numerals_to_decimal("V") == 5);
+    REQUIRE(roman_numerals_to_decimal("VI") == 6);
 
+    REQUIRE(roman_numerals_to_decimal("IX") == 9);
     REQUIRE(roman_numerals_to_decimal("X") == 10);
     REQUIRE(roman_numerals_to_decimal("XX") == 20);
     REQUIRE(roman_numerals_to_decimal("XXX") == 30);
+    REQUIRE(roman_numerals_to_decimal("XXXI") == 31);
+    REQUIRE(roman_numerals_to_decimal("XXXV") == 35);
+    REQUIRE(roman_numerals_to_decimal("XXXVI") == 36);
 
+    REQUIRE(roman_numerals_to_decimal("XL") == 40);
+    REQUIRE(roman_numerals_to_decimal("XLIX") == 49);
     REQUIRE(roman_numerals_to_decimal("L") == 50);
+    REQUIRE(roman_numerals_to_decimal("LI") == 51);
+    REQUIRE(roman_numerals_to_decimal("LV") == 55);
+    REQUIRE(roman_numerals_to_decimal("LX") == 60);
+    REQUIRE(roman_numerals_to_decimal("LXV") == 65);
+    REQUIRE(roman_numerals_to_decimal("LXVI") == 66);
 
+    REQUIRE(roman_numerals_to_decimal("XC") == 90);
+    REQUIRE(roman_numerals_to_decimal("XCIX") == 99);
     REQUIRE(roman_numerals_to_decimal("C") == 100);
+    REQUIRE(roman_numerals_to_decimal("XCXL") == 130);
+    REQUIRE(roman_numerals_to_decimal("XCXLIX") == 139);
     REQUIRE(roman_numerals_to_decimal("CC") == 200);
     REQUIRE(roman_numerals_to_decimal("CCC") == 300);
+    REQUIRE(roman_numerals_to_decimal("CCCI") == 301);
+    REQUIRE(roman_numerals_to_decimal("CCCV") == 305);
+    REQUIRE(roman_numerals_to_decimal("CCCX") == 310);
+    REQUIRE(roman_numerals_to_decimal("CCCL") == 350);
+    REQUIRE(roman_numerals_to_decimal("CCCLX") == 360);
+    REQUIRE(roman_numerals_to_decimal("CCCLXV") == 365);
+    REQUIRE(roman_numerals_to_decimal("CCCLXVI") == 366);
 
+    REQUIRE(roman_numerals_to_decimal("CD") == 400);
+    REQUIRE(roman_numerals_to_decimal("CDXL") == 440);
+    REQUIRE(roman_numerals_to_decimal("CDXLIX") == 449);
     REQUIRE(roman_numerals_to_decimal("D") == 500);
+    REQUIRE(roman_numerals_to_decimal("DI") == 501);
+    REQUIRE(roman_numerals_to_decimal("DV") == 505);
+    REQUIRE(roman_numerals_to_decimal("DX") == 510);
+    REQUIRE(roman_numerals_to_decimal("DL") == 550);
+    REQUIRE(roman_numerals_to_decimal("DC") == 600);
+    REQUIRE(roman_numerals_to_decimal("DCL") == 650);
+    REQUIRE(roman_numerals_to_decimal("DCLX") == 660);
+    REQUIRE(roman_numerals_to_decimal("DCLXV") == 665);
+    REQUIRE(roman_numerals_to_decimal("DCLXVI") == 666);
 
+    REQUIRE(roman_numerals_to_decimal("CM") == 900);
+    REQUIRE(roman_numerals_to_decimal("CMXC") == 990);
+    REQUIRE(roman_numerals_to_decimal("CMXCIX") == 999);
     REQUIRE(roman_numerals_to_decimal("M") == 1000);
+    REQUIRE(roman_numerals_to_decimal("CMCD") == 1300);
+    REQUIRE(roman_numerals_to_decimal("CMCDXC") == 1390);
+    REQUIRE(roman_numerals_to_decimal("CMCDXCXL") == 1430);
+    REQUIRE(roman_numerals_to_decimal("CMCDXCXLIX") == 1439);
     REQUIRE(roman_numerals_to_decimal("MM") == 2000);
     REQUIRE(roman_numerals_to_decimal("MMM") == 3000);
+    REQUIRE(roman_numerals_to_decimal("MMMI") == 3001);
+    REQUIRE(roman_numerals_to_decimal("MMMV") == 3005);
+    REQUIRE(roman_numerals_to_decimal("MMMX") == 3010);
+    REQUIRE(roman_numerals_to_decimal("MMML") == 3050);
+    REQUIRE(roman_numerals_to_decimal("MMMC") == 3100);
+    REQUIRE(roman_numerals_to_decimal("MMMD") == 3500);
+    REQUIRE(roman_numerals_to_decimal("MMMDC") == 3600);
+    REQUIRE(roman_numerals_to_decimal("MMMDCL") == 3650);
+    REQUIRE(roman_numerals_to_decimal("MMMDCLX") == 3660);
+    REQUIRE(roman_numerals_to_decimal("MMMDCLXV") == 3665);
+    REQUIRE(roman_numerals_to_decimal("MMMDCLXVI") == 3666);
 }
 
 TEST_CASE("Roman Numerals - Invalid", "[roman_numerals]") {
